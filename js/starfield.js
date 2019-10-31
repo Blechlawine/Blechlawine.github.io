272c419f8dfa828a1bbb79189001177d136841dc
Blechlawine <marczinser@gmx.de>
2019-10-31 14:05:16 +0100

    starfield update
    undefined canvas fixed
    still doesnt draw anything

--- js/starfield.js
+++ js/starfield.js
@@ -3,20 +3,23 @@ function startStarfield() {
     if(starFieldCanvas.getContext) {
         var starfield = new Starfield(starFieldCanvas);
         starfield.canvas.addEventListener("mousemove", starfield.onMouseMove);
-        var blah = setInterval(starfield.draw, 33);
+        var blah = setInterval(function() {
+            starfield.draw();
+        }, 33);
     }
 }
 
 class Starfield {
 
     constructor(canvasIn) {
-        this.canvas = canvasIn;
+        this.canvas = document.getElementById("starFieldCanvas");
         this.context = this.canvas.getContext("2d");
         this.canvasData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+        this.context.fillStyle = "#FF0000";
         this.stars = [];
         this.mousePos = (0, 0);
         for(var i = 0; i < 200; i++) {
-            this.stars.push(new Star(this.getRandomInt(this.canvas.width), this.getRandomInt(this.canvas.height), this.getRandomInt(20)));
+            this.stars.push(new Star(this.getRandomInt(this.canvas.width), this.getRandomInt(this.canvas.height), this.getRandomInt(30)));
         }
     }
 
@@ -33,11 +36,12 @@ class Starfield {
     }
 
     draw() {
-        console.log("Hello");
-        //if(this.canvas) {
-
+        console.log(this.canvas);
+        if(this.canvas) {
             this.canvas.width = window.innerWidth;
             this.canvas.height = window.innerHeight;
+            this.context.fillStyle = "#FF0000";
+            this.context.strokeStyle = "#FF0000";
             for(var star in this.stars) {
                 this.context.beginPath();
                 this.context.arc(star.x, star.y, star.size, 0, 2 * Math.PI, false);
@@ -46,13 +50,13 @@ class Starfield {
             this.context.beginPath();
             this.context.lineTo(this.mousePos[0], this.mousePos[1]);
             this.context.stroke();
-        // }
+        }
     }
 
     onMouseMove(event) {
         var mouseX = event.clientX;
         var mouseY = event.clientY;
-        this.mousePos = (mouseX, mouseY);
+        this.mousePos = [mouseX, mouseY];
     }
 }
 
